685de8eca9ba99b0ea4c06b4356b9e2e003f1317
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-09-14 05:04:06 -0700

    init

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* Main -Entry point
+*Return: Always 0 (Success)
+*/
+
+void print_alphabet (void)
+{
+int alpha;
+for (alpha = 97; alpha < 122; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+}
