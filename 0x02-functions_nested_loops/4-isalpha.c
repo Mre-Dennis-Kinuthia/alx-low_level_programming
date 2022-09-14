91a256fc2cce522aa180be03efed8d7338ecbdc0
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-09-14 10:12:56 -0700

    init

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
-* _isalpha description
+*  _isalpha - Shows 1 if the input is a
+* letter Another cases, shows 0
+*
 * @c : character to check
+*
 * Return:0 or 1
 */
 int _isalpha(int c)
