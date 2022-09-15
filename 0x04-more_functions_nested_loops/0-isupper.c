03ed44ad93222337ca5da4a00d1899e77e058f9a
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-09-15 09:28:25 -0700

    init

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,19 +3,15 @@
 
 
 /**
-
+*
 * _isupper - checks if the character is uppercase
-
+*
 * @c: is the int used for the argument of the function.
-
+*
 * Return: 1 if uppercase character 0 if not
-
+*
 */
-
-
-
 int _isupper(int c)
-
 {
 
 if (c >= 'A' && c <= 'Z')
