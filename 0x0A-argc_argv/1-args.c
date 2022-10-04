8b60797caecf9d7c0f8133afc9264d2970097967
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-10-04 04:38:38 -0700

    init

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
 * @argv: array that contains the program command line arguments
 * Return: 0 - success.
 */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 
