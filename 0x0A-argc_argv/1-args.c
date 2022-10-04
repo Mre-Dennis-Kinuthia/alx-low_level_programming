e0f71bc54a42d50d987c72a01b2037071caff6ed
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-10-04 04:27:13 -0700

    init

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - prints its name, followed by a new line.
+* @argc: number of command line arguments.
+* @argv: array that contains the program command line arguments
+* Return: 0 - success.
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+int i;
+for(i=0; i<argc; i++)
+printf("\nargv[%d]: %s", i,argv[i]);
+return (0);
+}
