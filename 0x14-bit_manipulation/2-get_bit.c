f9d09a5bd188cc9234e2ada59210b7fed1183a8a
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-11-01 09:33:17 -0700

    init

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index == 64 || n == 0)
-	{
+	if (index >= 64 || n == 0)
 		return (-1);
-	}
+
 	if ((n & (1 << index)) == 0)
-	{
 		return (0);
-	}
+
 	return (1);
 }
