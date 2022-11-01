30b2e964b2d09ccf2da66cec5811f2dc1ba9f247
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-11-01 09:02:03 -0700

    init

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* get_endianness - checks endianness
+* Return: nothing
+*/
+
+int get_endianness(void)
+{
+	unsigned int x = 0x76543210;
+	char *c = (char *) &x;
+	if (*c == 0x10)
+		return (1);
+	else
+		return (0);
+}
