c226da4fe053ee69c7bb058fc89eb8343581d67c
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2022-11-01 09:28:19 -0700

    init

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,11 +13,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int num;
 	unsigned int count = 0;
+
 	num = n ^ m;
+
 	while (num)
 	{
-	count += num & 1;
-	num = num >> 1;
+		count += num & 1;
+		num = num >> 1;
 	}
 	return (count);
 }
