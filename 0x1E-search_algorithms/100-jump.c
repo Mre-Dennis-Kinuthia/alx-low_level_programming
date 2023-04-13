b3e94d51ce4690bd4b2a424fc2ee290bb0b96fc1
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-14 00:47:27 +0300

    jump

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,27 @@
+#include <math.h>
+
+int jump_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return -1;
+
+    int jump = sqrt(size);
+    int left = 0, right = 0;
+
+    while (right < (int)size && array[right] < value) {
+        left = right;
+        right += jump;
+        printf("Value checked array[%d] = [%d]\n", left, array[left]);
+    }
+
+    right = (right > (int)size - 1) ? (int)size - 1 : right;
+    printf("Value found between indexes [%d] and [%d]\n", left, right);
+
+    for (int i = left; i <= right; i++) {
+        printf("Value checked array[%d] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return i;
+    }
+
+    return -1;
+}
