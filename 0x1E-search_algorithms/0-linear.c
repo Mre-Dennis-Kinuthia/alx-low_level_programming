c9fbbd346c98a04bf3134d7c6d12e3f54ef99d6e
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-13 22:44:25 +0300

    init

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,21 +2,21 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-  /* Check if array pointer is NULL */
-    if (array == NULL)
-        return -1;
+	/* Check if array pointer is NULL */
+	if (array == NULL)
+		return (-1);
 
-    /* Iterate over the array using a for loop */
-    for(size_t i = 0; i < size; i++)
-    {
-      /* Print the value of the current element being checked */
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        
-        /* Check if the current element matches the value we're searching for */
-        if (array[i] == value)
-            return i;
-    }
+	/* Iterate over the array using a for loop */
+	for (size_t i = 0; i < size; i++)
+	{
+		/* Print the value of the current element being checked */
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-    /* If the function reaches this point, the value was not found in the array */
-    return -1;
+		/* Check if the current element matches the value we're searching for */
+		if (array[i] == value)
+			return (i);
+	}
+
+	/* If the function reaches this point, the value was not found in the array */
+	return (-1);
 }
