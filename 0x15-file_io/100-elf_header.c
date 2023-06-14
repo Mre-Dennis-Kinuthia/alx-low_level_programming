e087e9e6e2a50c2c85b6dd946a5f2b3cf6ff6014
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-06-14 23:55:16 +0300

    init

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
  * main - Copies the content of a file to another file.
- * @ac: Argument count
- * @av: argument values
- * Return: 0 on succes, -1 on error.
+ * @argc: Argument count
+ * @argv: Argument values
+ * Return: 0 on success, -1 on error.
  */
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
-return (1);
-return (0);
+    (void)argc;
+    (void)argv;
+
+    return (0);
 }
