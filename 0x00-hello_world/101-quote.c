a4a90ddf91104b795b7bfdacbd3e3007c346a931
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-06-15 15:44:51 +0300

    init

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/* Prints string */
+
+#include <unistd.h>
+
+/**
+ * main - program starts here
+ *Return: print string; otherwise 1
+ */
+
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* Prints string */
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - function contains main program code
+ * Return: print string; 0 otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
