386a55117b09705624edc615a9b398c0b1de0e86
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-06-15 12:35:52 +0300

    init

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -q -P /tmp/ https://raw.githubusercontent.com/Michael-Maina/alx-low_level_programming/master/0x18-dynamic_libraries/make_me_win.so;
-export LD_PRELOAD=/tmp/make_me_win.so
